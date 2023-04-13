25a4b43b47b88d13d238a3e955982168ad91a3c3
Julio <Jumaga2015@gmail.com>
2023-04-13 18:48:30 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Hello, World!");
+
+X509Store store = new X509Store(StoreName.TrustedPeople, StoreLocation.LocalMachine);
+store.Open(OpenFlags.ReadOnly);
+foreach (X509Certificate2 certificate in store.Certificates)
+{
+    Console.WriteLine(certificate.ToString());
+}
+
+Console.ReadLine();
